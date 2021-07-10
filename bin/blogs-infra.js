279403e38f9fd4a46552d306287edd81328d2604
soumik-mukherjee <60540758+soumik-mukherjee@users.noreply.github.com>
2021-07-10 17:47:46 +0530

    wip:backup from wsl2

--- bin/blogs-infra.js
+++ bin/blogs-infra.js
@@ -4,7 +4,7 @@ const cdk = require('@aws-cdk/core');
 const { BlogsInfraStack } = require('../lib/blogs-infra-stack');
 
 const app = new cdk.App();
-new BlogsInfraStack(app, 'BlogsInfraStack', {
+const previewStack = new BlogsInfraStack(app, 'BlogsInfraStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new BlogsInfraStack(app, 'BlogsInfraStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '354603252852', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
